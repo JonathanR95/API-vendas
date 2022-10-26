450de2a3c996259ce263c1c347b3e8c4d93f34ed
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-10-26 15:43:42 -0300

    Criação dos métodos getAll, GetById e GetByName

--- src/main/java/com/turn2c/apivendas/controller/VendedorController.java
+++ src/main/java/com/turn2c/apivendas/controller/VendedorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,18 @@ public class VendedorController {
 	public ResponseEntity<List<Vendedor>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Vendedor> GetById(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity <List<Vendedor>> GetByName(@PathVariable String nome){
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	
 }
