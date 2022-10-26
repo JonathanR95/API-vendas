48cc809c6bbeedd2501917f01ffd0e25a9d0257b
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-10-26 10:22:06 -0300

    criando o projeto springboot

---
+++ src/main/java/com/turn2c/apivendas/ApivendasApplication.java
@@ -0,0 +1,13 @@
+package com.turn2c.apivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turn2c/apivendas/ApivendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turn2c.apivendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApivendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
