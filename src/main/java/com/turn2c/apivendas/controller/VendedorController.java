468be865b1f2bce09a68cc335c6a1aca38389166
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-10-26 17:03:46 -0300

    criação dos métodos post e delete do vendedorController

--- src/main/java/com/turn2c/apivendas/controller/VendedorController.java
+++ src/main/java/com/turn2c/apivendas/controller/VendedorController.java
@@ -3,10 +3,14 @@ package com.turn2c.apivendas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,5 +42,14 @@ public class VendedorController {
 		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Vendedor> post(@RequestBody Vendedor vendedor){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(vendedor));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 	
 }
