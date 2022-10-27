7a8bc072ca27109d95c8bd4aed1c401a80aef6ce
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-10-26 22:19:58 -0300

    Criação da interface ClienteRepository e da Classe ClienteController com os metodos get, getbyid e getbyname

--- src/main/java/com/turn2c/apivendas/repository/ClienteRepository.java
+++ src/main/java/com/turn2c/apivendas/repository/ClienteRepository.java
@@ -1,5 +1,11 @@
 package com.turn2c.apivendas.repository;
 
-public interface ClienteRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.turn2c.apivendas.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	public List<Cliente> findAllByNomeContainingIgnoreCase(String nome);
 }
